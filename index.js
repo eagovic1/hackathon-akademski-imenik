d4a4620ea879bbd76b0d83148c457e0b15290b78
eagovic1 <eagovic1@etf.unsa.ba>
2024-02-25 09:09:57 +0100

    merge routes in index.js

--- index.js
+++ index.js
@@ -1,16 +1,119 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const fs = require('fs');
-const OpenAI = require('openai');
-const path = require('path');
-
-const openai = new OpenAI({apiKey : "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"} )
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const OpenAI = require("openai");
+const request = require('request');
+const pdf = require('pdf-parse');
+const pdfUrl = 'your-pdf-url';
+const openai = new OpenAI({
+    apiKey: "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"
+});
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000);
+const downloadPDF = (url) => {
+    return new Promise((resolve, reject) => {
+        request({ uri: url, encoding: null }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                resolve(body);
+            } else {
+                reject(error);
+            }
+        });
+    });
+};
+
+const readPDF = async (url) => {
+    try {
+        const pdfBuffer = await downloadPDF(url);
+        const data = await pdf(pdfBuffer);
+
+        return data.text;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+async function getExtractInfo(text) {
+    const stream = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ role: 'user', content: `Extract the following information from the text:\nTitle:\nVenue:\nAuthors:\nEmails:\nAffiliations:\nProjects:\n${text}` }],
+    });
+    console.log(stream.choices[0])
+    return stream.choices[0].message.content;
+}
+
+
+async function getAnswer(question, type = "", lang = "") {
+    let prompt = "";
+    if (type == null || type == "" || type == ":type") {
+        prompt = `Write me 5 to 10 sentences summary of this research paper in english language and (IMPORTANT) general terminology and (FOLLOW TYPE OF TERMINOLOGY STRICTLY): ${question}`
+    }
+    else prompt = `Write me 5 to 10 sentences summary of this research paper in ${lang} language and (IMPORTANT) ${type} terminology and (FOLLOW TYPE OF TERMINOLOGY STRICTLY): ${question}`
+
+    const stream = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+
+        messages: [
+            {
+                role: "user",
+                content: prompt,
+
+            },
+        ],
+        temperature: 0.25,
+    });
+    return stream.choices[0].message.content;
+}
+
+app.get("/summary/:id/:type/:lang", function (req, res) {
+    console.time("test_timer");
+    if (
+        !["medical", "scientific", "technical", "simplified", "general", ":type", ""].includes(
+            req.params.type.toString()
+        ) ||
+        !["english", "german", "bosnian", ":lang", ""].includes(req.params.lang.toString())
+    ) {
+        res.send("Invalid type or language");
+        return;
+    }
+    axios
+        .get(
+            `https://api.semanticscholar.org/graph/v1/paper/${req.params.id}?openAccessPdf=true&fields=openAccessPdf`
+        )
+        .then(async function (response) {
+            const url = response.data.openAccessPdf.url;
+            console.log(response);
+
+            const read = await readPDF(response.data.openAccessPdf.url);
+            const answer = await getAnswer(read, req.params.type, req.params.lang);
+            res.send(answer);
+            console.timeEnd("test_timer");
+        })
+        .catch(function (error) {
+            console.log("er", error);
+        })
+        .finally(function () { });
+});
+
+app.get('/info/:id', function (req, res) {
+    axios.get(`https://api.semanticscholar.org/graph/v1/paper/${req.params.id}?openAccessPdf=true&fields=openAccessPdf`)
+        .then(async function (response) {
+            const url = response.data.openAccessPdf.url;
+            console.log(response.data.openAccessPdf.url);
+            const read = await readPDF(response.data.openAccessPdf.url);
+            const answer = await getExtractInfo(read);
+            res.send(answer);
+        }).catch(function (error) {
+            console.log(error);
+        })
+})
+
+app.listen(3000, () => {
+    console.log("Started");
+});
 
